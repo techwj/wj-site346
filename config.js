ab5e88a2df46f054736b8076b1f557a980c43d02
aotushi <weijutech@outlook.com>
2026-04-23 15:41:48 +0800

    Update ad integration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SITE_CONFIG = {
-  domain: "https://admptec.com",
+  domain: "https://game77.quickshipin.com",
   siteName: "H5 Free GameBox",
   description:
     "Play 200+ free H5 games online. No download, no login required. Puzzle, action, sports and more!",
