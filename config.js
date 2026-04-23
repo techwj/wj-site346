0a50dae0e2bc223dd26ace3a06cb1b5f9dad586d
aotushi <weijutech@outlook.com>
2026-04-23 18:34:37 +0800

    Update remaining old domains in config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.SITE_CONFIG = {
   description:
     "Play 200+ free H5 games online. No download, no login required. Puzzle, action, sports and more!",
   keywords: "free h5 games, online games, browser games, puzzle games, casual games, play free",
-  ogImage: "https://admptec.com/webp/2048-remastered.webp",
+  ogImage: "https://game77.quickshipin.com/webp/2048-remastered.webp",
 };
